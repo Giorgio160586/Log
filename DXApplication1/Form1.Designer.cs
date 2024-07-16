228e0075a512b7ef69a9b442cbd691170d9274b1
Giorgio <giorgio.saad@swisslog-healthcare.com>
2024-07-16 08:37:00 +0200

    Add project files.

---
+++ DXApplication1/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace DXApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ribbonControl = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.skinRibbonGalleryBarItem = new DevExpress.XtraBars.SkinRibbonGalleryBarItem();
+            this.skinDropDownButtonItem = new DevExpress.XtraBars.SkinDropDownButtonItem();
+            this.skinPaletteRibbonGalleryBarItem = new DevExpress.XtraBars.SkinPaletteRibbonGalleryBarItem();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.barStaticItem1 = new DevExpress.XtraBars.BarStaticItem();
+            this.barEditItem1 = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemTextEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemTextEdit();
+            this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+            this.barEditItem2 = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemTextEdit2 = new DevExpress.XtraEditors.Repository.RepositoryItemTextEdit();
+            this.barEditItem3 = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemTextEdit3 = new DevExpress.XtraEditors.Repository.RepositoryItemTextEdit();
+            this.barEditItem4 = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemTextEdit4 = new DevExpress.XtraEditors.Repository.RepositoryItemTextEdit();
+            this.ribbonPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonControl
+            // 
+            this.ribbonControl.EmptyAreaImageOptions.ImagePadding = new System.Windows.Forms.Padding(2436739, 2506403, 2436739, 2506403);
+            this.ribbonControl.ExpandCollapseItem.Id = 0;
+            this.ribbonControl.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl.ExpandCollapseItem,
+            this.skinRibbonGalleryBarItem,
+            this.skinDropDownButtonItem,
+            this.skinPaletteRibbonGalleryBarItem,
+            this.barButtonItem1,
+            this.barStaticItem1,
+            this.barEditItem1,
+            this.barButtonItem2,
+            this.barEditItem2,
+            this.barEditItem3,
+            this.barEditItem4});
+            this.ribbonControl.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl.Margin = new System.Windows.Forms.Padding(252760, 269046, 252760, 269046);
+            this.ribbonControl.MaxItemId = 9;
+            this.ribbonControl.Name = "ribbonControl";
+            this.ribbonControl.OptionsMenuMinWidth = 27067688;
+            this.ribbonControl.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage});
+            this.ribbonControl.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemTextEdit1,
+            this.repositoryItemTextEdit2,
+            this.repositoryItemTextEdit3,
+            this.repositoryItemTextEdit4});
+            this.ribbonControl.ShowDisplayOptionsMenuButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.Size = new System.Drawing.Size(1673, 327);
+            // 
+            // skinRibbonGalleryBarItem
+            // 
+            this.skinRibbonGalleryBarItem.Caption = "skinRibbonGalleryBarItem";
+            this.skinRibbonGalleryBarItem.Id = 1;
+            this.skinRibbonGalleryBarItem.Name = "skinRibbonGalleryBarItem";
+            // 
+            // skinDropDownButtonItem
+            // 
+            this.skinDropDownButtonItem.Id = 2;
+            this.skinDropDownButtonItem.Name = "skinDropDownButtonItem";
+            // 
+            // skinPaletteRibbonGalleryBarItem
+            // 
+            this.skinPaletteRibbonGalleryBarItem.Caption = "skinPaletteRibbonGalleryBarItem";
+            this.skinPaletteRibbonGalleryBarItem.Id = 3;
+            this.skinPaletteRibbonGalleryBarItem.Name = "skinPaletteRibbonGalleryBarItem";
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "FIND ALL";
+            this.barButtonItem1.Id = 2;
+            this.barButtonItem1.Name = "barButtonItem1";
+            this.barButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem1_ItemClick);
+            // 
+            // barStaticItem1
+            // 
+            this.barStaticItem1.Caption = "barStaticItem1";
+            this.barStaticItem1.Id = 3;
+            this.barStaticItem1.Name = "barStaticItem1";
+            // 
+            // barEditItem1
+            // 
+            this.barEditItem1.Caption = "Find 1:";
+            this.barEditItem1.Edit = this.repositoryItemTextEdit1;
+            this.barEditItem1.EditWidth = 300;
+            this.barEditItem1.Id = 4;
+            this.barEditItem1.Name = "barEditItem1";
+            // 
+            // repositoryItemTextEdit1
+            // 
+            this.repositoryItemTextEdit1.AutoHeight = false;
+            this.repositoryItemTextEdit1.Name = "repositoryItemTextEdit1";
+            // 
+            // barButtonItem2
+            // 
+            this.barButtonItem2.Caption = "CLEAR";
+            this.barButtonItem2.Id = 5;
+            this.barButtonItem2.Name = "barButtonItem2";
+            this.barButtonItem2.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem2_ItemClick);
+            // 
+            // barEditItem2
+            // 
+            this.barEditItem2.Caption = "barEditItem2";
+            this.barEditItem2.Edit = this.repositoryItemTextEdit2;
+            this.barEditItem2.Id = 6;
+            this.barEditItem2.Name = "barEditItem2";
+            // 
+            // repositoryItemTextEdit2
+            // 
+            this.repositoryItemTextEdit2.AutoHeight = false;
+            this.repositoryItemTextEdit2.Name = "repositoryItemTextEdit2";
+            // 
+            // barEditItem3
+            // 
+            this.barEditItem3.Caption = "Find 2:";
+            this.barEditItem3.Edit = this.repositoryItemTextEdit3;
+            this.barEditItem3.EditWidth = 300;
+            this.barEditItem3.Id = 7;
+            this.barEditItem3.Name = "barEditItem3";
+            // 
+            // repositoryItemTextEdit3
+            // 
+            this.repositoryItemTextEdit3.AutoHeight = false;
+            this.repositoryItemTextEdit3.Name = "repositoryItemTextEdit3";
+            // 
+            // barEditItem4
+            // 
+            this.barEditItem4.Caption = "Find 3:";
+            this.barEditItem4.Edit = this.repositoryItemTextEdit4;
+            this.barEditItem4.EditWidth = 300;
+            this.barEditItem4.Id = 8;
+            this.barEditItem4.Name = "barEditItem4";
+            // 
+            // repositoryItemTextEdit4
+            // 
+            this.repositoryItemTextEdit4.AutoHeight = false;
+            this.repositoryItemTextEdit4.Name = "repositoryItemTextEdit4";
+            // 
+            // ribbonPage
+            // 
+            this.ribbonPage.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+            this.ribbonPage.Name = "ribbonPage";
+            this.ribbonPage.Text = "Main";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.AllowTextClipping = false;
+            this.ribbonPageGroup1.ItemLinks.Add(this.barEditItem1);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barEditItem3);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barEditItem4);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem1);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem2);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.memoEdit1.Location = new System.Drawing.Point(0, 327);
+            this.memoEdit1.Margin = new System.Windows.Forms.Padding(119576);
+            this.memoEdit1.MenuManager = this.ribbonControl;
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Properties.Appearance.Font = new System.Drawing.Font("Consolas", 11F);
+            this.memoEdit1.Properties.Appearance.Options.UseFont = true;
+            this.memoEdit1.Properties.Appearance.Options.UseTextOptions = true;
+            this.memoEdit1.Properties.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.NoWrap;
+            this.memoEdit1.Properties.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.memoEdit1.Properties.UseAdvancedMode = DevExpress.Utils.DefaultBoolean.True;
+            this.memoEdit1.Properties.WordWrap = false;
+            this.memoEdit1.Size = new System.Drawing.Size(1673, 754);
+            this.memoEdit1.TabIndex = 2;
+            this.memoEdit1.CustomHighlightText += new DevExpress.XtraEditors.TextEditCustomHighlightTextEventHandler(this.memoEdit1_CustomHighlightText);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1673, 1081);
+            this.Controls.Add(this.memoEdit1);
+            this.Controls.Add(this.ribbonControl);
+            this.IconOptions.Image = ((System.Drawing.Image)(resources.GetObject("Form1.IconOptions.Image")));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Ribbon = this.ribbonControl;
+            this.Text = "Log++";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage;
+        private DevExpress.XtraBars.SkinRibbonGalleryBarItem skinRibbonGalleryBarItem;
+        private DevExpress.XtraBars.SkinDropDownButtonItem skinDropDownButtonItem;
+        private DevExpress.XtraBars.SkinPaletteRibbonGalleryBarItem skinPaletteRibbonGalleryBarItem;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarStaticItem barStaticItem1;
+        private DevExpress.XtraBars.BarEditItem barEditItem1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemTextEdit repositoryItemTextEdit1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.BarEditItem barEditItem2;
+        private DevExpress.XtraEditors.Repository.RepositoryItemTextEdit repositoryItemTextEdit2;
+        private DevExpress.XtraBars.BarEditItem barEditItem3;
+        private DevExpress.XtraEditors.Repository.RepositoryItemTextEdit repositoryItemTextEdit3;
+        private DevExpress.XtraBars.BarEditItem barEditItem4;
+        private DevExpress.XtraEditors.Repository.RepositoryItemTextEdit repositoryItemTextEdit4;
+    }
+}
---
+++ DXApplication1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using DevExpress.LookAndFeel;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraEditors.Repository;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DXApplication1
+{
+    public partial class Form1 : RibbonForm
+    {
+        public string text { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+
+            repositoryItemTextEdit1.Appearance.ForeColor = DXSkinColors.FillColors.Question;
+            repositoryItemTextEdit3.Appearance.ForeColor = DXSkinColors.FillColors.Warning;
+            repositoryItemTextEdit4.Appearance.ForeColor = DXSkinColors.FillColors.Success;
+       
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Log++"))
+            {
+                if (key != null)
+                {
+                    barEditItem1.EditValue = key.GetValue("FindAll1");
+                    barEditItem3.EditValue = key.GetValue("FindAll2");
+                    barEditItem4.EditValue = key.GetValue("FindAll3");
+                    memoEdit1.EditValue = key.GetValue("Text");
+                }
+            }
+
+        }
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Log++"))
+            {
+                key.SetValue("FindAll1", Convert.ToString(barEditItem1.EditValue), RegistryValueKind.String);
+                key.SetValue("FindAll2", Convert.ToString(barEditItem3.EditValue), RegistryValueKind.String);
+                key.SetValue("FindAll3", Convert.ToString(barEditItem4.EditValue), RegistryValueKind.String);
+                key.SetValue("Text", Convert.ToString(memoEdit1.EditValue), RegistryValueKind.String);
+            }
+
+            text = memoEdit1.Text;
+            var l = memoEdit1.Text.Split('\n').ToArray();
+            var filters = new List<string>();
+
+            if (!string.IsNullOrEmpty(Convert.ToString(barEditItem1.EditValue)))
+                filters.Add(Convert.ToString(barEditItem1.EditValue));
+            if (!string.IsNullOrEmpty(Convert.ToString(barEditItem3.EditValue)))
+                filters.Add(Convert.ToString(barEditItem3.EditValue));
+            if (!string.IsNullOrEmpty(Convert.ToString(barEditItem4.EditValue)))
+                filters.Add(Convert.ToString(barEditItem4.EditValue));
+
+            if (filters.Count > 0)
+                l = l.Where(f => filters.Any(filter => f.Contains(filter))).ToArray();
+
+            memoEdit1.Text = string.Join("\n", l);
+        }
+        private void memoEdit1_CustomHighlightText(object sender, DevExpress.XtraEditors.TextEditCustomHighlightTextEventArgs e)
+        {
+            e.HighlightWords(Convert.ToString(barEditItem1.EditValue), DXSkinColors.FillColors.Question);
+            e.HighlightWords(Convert.ToString(barEditItem3.EditValue), DXSkinColors.FillColors.Warning);
+            e.HighlightWords(Convert.ToString(barEditItem4.EditValue), DXSkinColors.FillColors.Success);
+        }
+
+        private void barButtonItem2_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            memoEdit1.Text = text;
+        }
+    }
+}
